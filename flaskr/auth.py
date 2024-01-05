c4f3d366935a385adcfeb121ac3d244b505938d5
Italo Nabuco <italonabuco@hotmail.com>
2024-01-04 23:13:28 -0300

    feat: `login_required` decorator

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -91,3 +91,14 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for("index"))
+
+
+# decorator login required
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+
+    return wrapped_view
